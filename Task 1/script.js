70fb915de7489f54c08cca7ebdfae5084b062450
Daniel Dmitrenko <dandmitrenko@gmail.com>
2022-08-10 22:42:42 +0300

    Task 1 - complete

--- Task 1/script.js
+++ Task 1/script.js
@@ -1,13 +1,3 @@
-const ruEng = {
-  понедельник: 'monday',
-  вторник: 'tuesday',
-  среда: 'wednesday',
-  четверг: 'thursday',
-  пятница: 'friday',
-  суббота: 'satturday',
-  воскресенье: 'sunday',
-};
-
 let str = `Старший братец ПОНЕДЕЛЬНИК –
 работяга, не бездельник.
 Он неделю открывает
@@ -29,13 +19,71 @@ let str = `Старший братец ПОНЕДЕЛЬНИК –
 не выходит на работу.
 
 В гости ходит ВОСКРЕСЕНЬЕ,
-очень любит угощенье
+очень любит угощенье.
 `;
 
-for (let [key, value] of Object.entries(ruEng)) {
-  key = key.toUpperCase();
-  value = value.toUpperCase();
-  str.indexOf(key) && (str = str.replace(key, value));
+const ruEng = {
+  понедельник: 'monday',
+  вторник: 'tuesday',
+  среда: 'wednesday',
+  четверг: 'thursday',
+  пятница: 'friday',
+  суббота: 'satturday',
+  воскресенье: 'sunday',
+};
+
+// Вариант 1
+
+// for (let [key, value] of Object.entries(ruEng)) {
+//   key = key.toUpperCase();
+//   value = value.toUpperCase();
+//   str = str.replace(key, value);
+// }
+
+// console.log(str);
+
+// Вариант 2
+
+function changeString(langPair, initStr) {
+  let changedStr = initStr;
+
+  for (let [key, value] of Object.entries(langPair)) {
+    const foundStrings = changedStr.match(new RegExp(`${key}`, `gi`));
+
+    // С помощью if:
+    /*
+    if (foundStrings) {
+      foundStrings.forEach((el) => {
+        let resultValue;
+
+        if (key.toLowerCase() === el) {
+          resultValue = value.toLowerCase();
+        } else if (key.toUpperCase() === el) {
+          resultValue = value.toUpperCase();
+        } else {
+          resultValue = value[0].toUpperCase() + value.slice(1).toLowerCase();
+        }
+
+        changedStr = changedStr.replace(new RegExp(`${key}`, `i`), resultValue);
+      });
+    }
+    */
+
+    // С помощью логических операторов:
+
+    foundStrings &&
+      foundStrings.forEach((el) => {
+        let resultValue;
+
+        (key.toLowerCase() === el && (resultValue = value.toLowerCase())) ||
+          (key.toUpperCase() === el && (resultValue = value.toUpperCase())) ||
+          (resultValue = value[0].toUpperCase() + value.slice(1).toLowerCase());
+
+        changedStr = changedStr.replace(new RegExp(`${key}`, `i`), resultValue);
+      });
+  }
+
+  return changedStr;
 }
 
-console.log(str);
+console.log(changeString(ruEng, str));
